df9ca4f637b5319c196656c129b5733477e7ba70
actenov <karthick.24may@gmail.com>
2019-12-11 09:44:30 +0530

    removed comments

--- src/main/java/steps/Constants.java
+++ src/main/java/steps/Constants.java
@@ -2,8 +2,8 @@ package steps;
 
 public class Constants {
 
-//	public static String ulr ="https://www.abhibus.com/";
-//	public static String mercuryurl="http://newtours.demoaut.com/";
+	public static String ulr ="https://www.abhibus.com/";
+	public static String mercuryurl="http://newtours.demoaut.com/";
 	public static String expectedTitle =
 	"Online Bus Ticket Booking - Flat Rs.500 Cash Back on Bus Booking";
 
